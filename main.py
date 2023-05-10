6729e7c7ca12ae3c61c95cd1112e707376d08136
Artur Paschoal <128155308+paschoal-artur@users.noreply.github.com>
2023-05-10 14:18:24 +0000

    code final

--- main.py
+++ main.py
@@ -2,11 +2,11 @@
 
 
 def largest_sum(numbers: list[int]) -> tuple[int, int]:
-    if numbers:
+    if len(numbers) > 1 :
         numbers.sort()
         popped_num = numbers.pop()
         popped_num2 = numbers.pop()
         print(f"({popped_num2}, {popped_num})")
+        return popped_num2, popped_num
     else:
-        print("None")
-    return popped_num, popped_num2
+        return None
